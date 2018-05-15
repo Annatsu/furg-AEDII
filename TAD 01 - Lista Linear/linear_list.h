62e288a19a5297d2b827a31ca3e3d3d57360600b
Brendon Pagano <brendonpagano@gmail.com>
2018-05-15 15:28:15 -0300

    Header File da Lista Linear terminado

--- TAD 01 - Lista Linear/linear_list.h
+++ TAD 01 - Lista Linear/linear_list.h
@@ -1,14 +1,21 @@
 #pragma once
 
 
-namespace tad {
+namespace adt {
 
+    /**
+     * @brief   An implementation of the List Abstract Data Type (ADT).
+     * 
+     * This implementation uses an array allocated upon the class's constructor call,
+     * not a linked list.
+     */
     class LinearList {
 
-        private:
-            int storage[];
-
         public:
+
+            /**
+             * @brief   LinearList class's default constructor.
+             */
             LinearList();
 
             /**
@@ -27,13 +34,42 @@ namespace tad {
              * @return  the removed element
              */
             int removeAt(int pos);
+
+            /**
+             * @brief   Clear the entire list, removing all of its elements.
+             * 
+             * @return  1 if the list was successfully cleared, 0 otherwise.
+             */
             char clear();
 
 
+            /**
+             * @brief   Access the list's first element.
+             * 
+             * @return  int
+             */
             int front();
+
+            /**
+             * @brief   Access the list's last element.
+             * 
+             * @return  int
+             */
             int back();
+
+            /**
+             * @brief   Access a specified element with bounds checking.
+             * 
+             * @return  the item at the provided position.
+             */
             int getItem(int pos);
 
+
+            /**
+             * @brief   The number of elements inside the list.
+             * 
+             * @return int
+             */
             int size();
 
     };
