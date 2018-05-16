9d04c6db47f2f60f3cb461af5913a71fc25be630
Brendon Pagano <brendonpagano@gmail.com>
2018-05-16 03:29:43 -0300

    Made the test for the general insertAt method

--- TAD 01 - Lista Linear/linear_list.test.cpp
+++ TAD 01 - Lista Linear/linear_list.test.cpp
@@ -24,16 +24,37 @@ int main() {
     for (int i = 0; i < 5; i++)
         list.insertAt(i + 1, i);
 
-    // Display the list info.
+    // Should display a range from 0 to 4.
     listInfo(list);
     std::cout << "----------" << std::endl;
+
+
+    // --------------
 
 
     // Clear the list
     list.clear();
 
 
-    // List the details again.
+    // Should display an empty list.
     listInfo(list);
+    std::cout << "----------" << std::endl;
+
+
+    // --------------
+
+
+    // Insert five items at the end of the list.
+    for (int i = 0; i < 5; i++)
+        list.insertAt(i + 1, i);
+
+    
+    // Insert an element in the middle of the list.
+    list.insertAt(3, 12);
+
+
+    // Should display a list of size 6 with an element 12 in the middle.
+    listInfo(list);
+    std::cout << "----------" << std::endl;
 
 }
