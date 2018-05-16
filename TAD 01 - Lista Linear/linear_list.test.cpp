6a4e82b989d579c3ed62a6d4ce163ce4fff82edb
Brendon Pagano <brendonpagano@gmail.com>
2018-05-16 00:38:29 -0300

    Made the tests more modular

--- TAD 01 - Lista Linear/linear_list.test.cpp
+++ TAD 01 - Lista Linear/linear_list.test.cpp
@@ -3,30 +3,37 @@
 #include <iostream>
 
 
+void listInfo(adt::LinearList& list) {
+    std::cout << "List size: " << list.size() << std::endl;
+
+    // Loop through the list.
+    for (int i = 1; i <= list.size(); i++)
+        std::cout << "List at index [" << i << "] = " << list.getItem(i) << std::endl;
+
+    std::cout << "First element: " << list.front() << std::endl;
+    std::cout << "Last element: " << list.back() << std::endl;
+}
+
+
 int main() {
     
     // Declares a new LinearList object.
     adt::LinearList list;
-
-
+    
     // Insert five items at the end of the list.
     for (int i = 0; i < 5; i++)
         list.insertAt(i + 1, i);
 
-    // Check if the size is 5
-    std::cout << "List size: " << list.size() << std::endl;
+    // Display the list info.
+    listInfo(list);
+    std::cout << "----------" << std::endl;
 
 
+    // Clear the list
+    list.clear();
 
-    // Loop through the list. Should print: range(0, 4)
-    for (int i = 1; i <= list.size(); i++)
-        std::cout << list.getItem(i) << std::endl;
 
-    
-    // Get list's first element
-    std::cout << "First element: " << list.front() << std::endl;
-
-    // Get list's last element
-    std::cout << "Last element: " << list.back() << std::endl;
+    // List the details again.
+    listInfo(list);
 
 }
