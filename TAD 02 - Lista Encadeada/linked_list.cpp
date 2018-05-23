eeeebc0c2b046874ec2351fe7043b02aa99ca00e
Brendon Pagano <brendonpagano@gmail.com>
2018-05-23 01:43:08 -0300

    Beggining implementation of the LinkedList's class logic

---
+++ TAD 02 - Lista Encadeada/linked_list.cpp
@@ -0,0 +1,31 @@
+/** 
+ *  @file       linked_list.cpp
+ *  @author     Brendon Pagano (annatsu)
+ *  @date       05/23/2018
+ *  @version    1.0
+ *  
+ *  @brief      A Linked List ADT implementation file.
+ *
+ */
+
+
+#include "linked_list.h"
+
+
+
+namespace adt {
+
+    // Constructors
+
+    LinkedList::LinkedList() :
+        list({ 3, nullptr }),
+        size(0) {}
+
+
+    // Public methods
+
+    int LinkedList::front() {
+        return list.info;
+    }
+
+}
